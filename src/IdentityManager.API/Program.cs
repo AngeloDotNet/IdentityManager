02bcfa41d7033f533129ea86fb9e9983cd42efa5
Angelo Pirola <angelo9871@gmail.com>
2025-02-28 00:01:22 +0100

    Update IdentityManager API for .NET 8 and configuration
    - Migrate project to .NET 8 SDK with nullable and implicit usings.
    - Implement ASP.NET Core web application setup in Program.cs, including CORS, JWT, identity, and SMTP configurations.
    - Add launchSettings.json for development environment settings.
    - Configure logging levels in appsettings.Development.json.
    - Enhance appsettings.json with logging, Kestrel limits, JWT, identity, SMTP options, and database connection strings.

---
+++ src/IdentityManager.API/Program.cs
@@ -0,0 +1,45 @@
+namespace IdentityManager.API;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var connectionString = builder.Configuration.GetDatabaseConnString("DefaultConnection");
+
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddCors(options => options.AddPolicy("cors", builder
+            => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
+
+        var jwtOptions = builder.Configuration.GetSettingsOptions<JwtOptions>(nameof(JwtOptions));
+        var identityOptions = builder.Configuration.GetSettingsOptions<NetIdentityOptions>(nameof(NetIdentityOptions));
+        var smtpOptions = builder.Configuration.GetSettingsOptions<SmtpOptions>(nameof(SmtpOptions));
+
+        builder.Services.AddRegisterServices<Program>(connectionString, jwtOptions, identityOptions)
+            .AddAuthorization(options =>
+            {
+                options.AddDefaultAuthorizationPolicy(); // Adds default authorization policies
+
+                // Here you can add additional authorization policies
+            });
+
+        builder.Services
+            .AddSwaggerConfiguration()
+            .AddRegisterOptions(builder.Configuration);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger().UseSwaggerUI();
+        }
+
+        app.UseCors("cors");
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseMapEndpoints();
+        app.Run();
+    }
+}
